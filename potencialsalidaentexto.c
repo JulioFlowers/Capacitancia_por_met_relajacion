66a730027d3c6ee406ea7661ad179b1bfdfdbc7e
Julio <julio_perez@ciencias.unam.mx>
2023-11-26 21:40:17 -0600

    ptmmarchyajala

--- potencialsalidaentexto.c
+++ potencialsalidaentexto.c
@@ -15,9 +15,6 @@ void guardarArrayEnArchivo(int filas, int columnas, double array[filas][columnas
         return;
     }
 
-    // Escribir las dimensiones del array en la primera línea
-    fprintf(archivo, "%d %d\n", filas, columnas);
-
     // Escribir los elementos del array
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < columnas; j++) {
@@ -43,10 +40,10 @@ esp = cuad/2;
 for (i = 0; i < N; i++) {
         for (j = 0; j < M; j++) {
             if (j<=cuad && i<=4*cuad || j<=3*cuad && i>=4*cuad ||j>=6*cuad && i<=4*cuad || j>=4*cuad && i>=4*cuad || j>=3*cuad && j<=4*cuad && i<=cuad ){
-            phi[i][j]= 5.0;      // Voltajes positivos
+            phi[i][j]= 10.0;      // Voltajes positivos
             }
             else if (j>= cuad + esp && j<=2*cuad + esp && i<= 2*cuad+esp || j>= 3*(cuad + esp) && j<=4*cuad + 3*esp && i<= 2*cuad+esp || j>= cuad +esp && j<= 5*cuad+esp && i>2*cuad+esp && i<=3*cuad+esp || j>=3*cuad+esp && j<=4*cuad+esp && i>=3*cuad+esp ){
-                  phi[i][j]=-5;       // Voltajes negativos
+                  phi[i][j]=-5.0;       // Voltajes negativos
             }
             else {  
                 phi[i][j]=0.0;
