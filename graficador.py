c926d30516956faa34f4ac69251d8b9c7a060ea0
Julio <julio_perez@ciencias.unam.mx>
2023-11-27 02:21:52 -0600

    holavengoacagarrepo

--- graficador.py
+++ graficador.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read data from the text file
-f = open("/home/jflws/Documentos/Proyecto Final Fis. Comp/potencialrust/mi_archivo.txt")
+f = open("mi_archivo.txt")
 data = np.loadtxt(f)
 
 # Create a heatmap using matplotlib
