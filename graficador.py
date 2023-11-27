fa0e18b1eac87a6d040e7a6847c43b7bb34afd48
Julio <julio_perez@ciencias.unam.mx>
2023-11-27 02:52:11 -0600

    deloperdidologanado

--- graficador.py
+++ graficador.py
@@ -1,6 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
+def archivo_texto_a_matriz(ruta_archivo):
+    # Abrir el archivo de texto
+    path = Path(__file__).parent / ruta_archivo
+    with path.open('r') as archivo:
+        # Leer líneas del archivo
+        lineas = archivo.readlines()
+
+        # Procesar las líneas y crear una matriz
+        matriz = [list(map(float, linea.strip().split( ))) for linea in lineas]
+
+        # Convertir la lista de listas a un array de NumPy
+        matriz = np.array(matriz)
+
+    return matriz
+
+# Ejemplo de uso
+ruta_archivo = 'miArchivo.txt'
+m1 = archivo_texto_a_matriz(ruta_archivo)
+
+print("Matriz:")
+print(m1)
+
+[Ey,Ex] = np.gradient(m1)
+
+plt.figure(figsize=(8, 6))
+plt.contourf(m1, 100, cmap='inferno')
+#plt.contour(m1, 15, cmap='inferno')
+plt.colorbar(label='Potencial eléctrico [V]')
+plt.quiver(-Ex,-Ey,scale=5)
+plt.title('Potencial eléctrico con círculo interno y marco externo')
+plt.xlabel('x [cm]')
+plt.ylabel('y [cm]')
+plt.savefig('potcirc.jpg', dpi=500)
+plt.show()
+
+"""
 # Read data from the text file
 f = open("mi_archivo.txt")
 data = np.loadtxt(f)
@@ -16,5 +53,5 @@ plt.ylabel('Y-axis')
 # Display the plot
 plt.title('Heatmap from Text File')
 plt.show()
-
+"""
 
